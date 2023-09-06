5296520d7effe68c87f50c80c1fe77b441d221d6
Aldrich Suratos <aldrich.suratos@ksk-anl.com>
2023-09-06 17:34:54 +0900

    no longer fails when data folder does not exist

--- recordweather.py
+++ recordweather.py
@@ -15,13 +15,15 @@ target = f'{currentdir}/data/{filename}.csv'
 
 bme280.load_calibration_params(bus, address)
 
-if not os.path.exists(target):
+if not os.path.exists(f'{currentdir}/data'):
+    os.mkdir(f'{currentdir}/data')
     with open(target, "a") as f:
         f.write("timestamp,temp,humidity,airpressure\n")
 
 with open(target, "a") as f:
-    timenow = datetime.datetime.now().strftime('%Y/%m/%d-%H:%M:%S')
+    # timenow = datetime.datetime.now().strftime('%Y/%m/%d-%H:%M:%S')
     bme280_data = bme280.sample(bus, address)
+    timenow = bme280_data.timestamp.strftime('%Y/%m/%d-%H:%M:%S')
     ambient_temperature = bme280_data.temperature
     humidity = bme280_data.humidity
     pressure = bme280_data.pressure
